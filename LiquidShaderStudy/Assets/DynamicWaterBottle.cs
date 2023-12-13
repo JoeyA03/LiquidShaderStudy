bf491da41d810d1ee3044593e38503a44e1eb7a6
JoeyA03 <Joey_a03@hotmail.com>
2023-12-13 08:09:27 -0500

    Bottle Liquid Shader
    Add bottle to scene, provided script to manipulate plane from the water.

--- LiquidShaderStudy/Assets/DynamicWaterBottle.cs
+++ LiquidShaderStudy/Assets/DynamicWaterBottle.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class DynamicWaterBottle : MonoBehaviour
 {
+    public GameObject bottle;
+    public GameObject plane;
+    public Material liquid;
+
+    public float bottleHeight;
+
+
+    Vector3 normal;
+    Vector3 pos;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +22,18 @@ public class DynamicWaterBottle : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        pos = plane.transform.position;
+        normal = plane.transform.up;
+
+        Vector3 planPos = bottle.transform.position + offset;
+
+        Vector3 a = bottle.transform.position;
+        Vector3 a = bottle.transform.position + transform.transformDrection(Vector3.up) * bottleHeight;
+
+        plane.transform.rotation = Quaternion.Euler(new Vector3(bottle.transform.rotation.x, bottle.transform.rotation.y, bottle.transform.rotation.z));
+
+        liquid.SetVector("_PlanePosition", pos);
+        liquid.SetVector("_PlaneNormal", normal);
         
     }
 }
